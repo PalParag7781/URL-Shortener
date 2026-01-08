5e9f3fbb669d1aad4c42b1cd9217313e59e04383
PalParag7781 <palparag7781@gmail.com>
2026-01-09 01:47:41 +0530

    Delete routes directory

--- routes/url.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const { GenerateNewShortURL} = require('../controllers/url')
-const router = express.Router()
-
-router.post('/', GenerateNewShortURL);
-module.exports= router
-
